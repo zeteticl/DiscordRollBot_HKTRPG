22701826983529ff7bc8c56d25c7d0e8e528ce61
zeteticl <cougar.l@hotmail.com>
2018-02-23 16:55:17 +0800

    test

--- index.js
+++ index.js
@@ -121,21 +121,12 @@ var Kitten = mongoose.model('Kitten', kittySchema);
 var silence = new Kitten({ name: 'Silence' });
 console.log(silence.name); // 'Silence'
 
-// NOTE: methods must be added to the schema before compiling it with mongoose.model()
-kittySchema.methods.speak = function () {
-  var greeting = this.name
-    ? "Meow name is " + this.name
-    : "I don't have a name";
-  console.log(greeting);
-}
-
-var Kitten = mongoose.model('Kitten', kittySchema);
 
 var fluffy = new Kitten({ name: 'fluffy' });
 //fluffy.speak(); // "Meow name is fluffy"
 
-
+	
   fluffy.save(function (err, fluffy) {
     if (err) return console.error(err);
-     console.log(fluffy);
+     console.log(Kitten);
   });
