e7834d6489af8be869258d62363dca31c63ef80b
zeteticl <cougar.l@hotmail.com>
2018-02-27 15:32:30 +0800

    q

--- modules/mongoose.js
+++ modules/mongoose.js
@@ -19,13 +19,15 @@ Switch.save(function (err) {if (err) console.log ('Error on save!' + err)});
 */
 
 
+
+
 //用來呼叫骰組,新增骰組的話,要寫條件式到下面呼叫 
 //格式是 exports.骰組檔案名字.function名
 function findmongoose(functionSwitch) {
 var findall = {};
 functionSwitch.find({},function (err, findall) {
 if (err) return console.error(err);
-console.log(findall);
+console.log('findmongoose = '+ findall);
 return findall;
 })
 }
