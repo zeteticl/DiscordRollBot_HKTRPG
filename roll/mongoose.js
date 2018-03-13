0b55992fe94ef47eff2edfae74a66214d89ded45
zeteticl <cougar.l@hotmail.com>
2018-03-13 19:01:44 +0800

    test

--- roll/mongoose.js
+++ roll/mongoose.js
@@ -6,8 +6,8 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 //////////////// 
 ////////////////////////////////////////		
 
-function findmongoose(id) {
-    mongodb.functionSwitch.findOne({ groupid: id }, function (err, data) {
+function findmongoose(id, functionname) {
+    mongodb.functionSwitch.findOne({ groupid: id,functionname: luck }, function (err, data) {
         if (err) {
             console.log(err);
             return
@@ -15,21 +15,11 @@ function findmongoose(id) {
 
         if (data == null) {
             console.log("No record found")
-            return 0;
+            return 1;
         }
-
         console.log('date = ' + data);
-        console.log('date _id = ' + data._id);
-        console.log('date functionswitch = ' + data.functionswitch);
-        console.log('date groupid = ' + data.groupid);
-        console.log('date [0] = ' + data[0]);
-        console.log('date [1] = ' + data[1]);
-        console.log('date [2] = ' + data[2]);
-        console.log('date [3] = ' + data[3]);
-
         return data.functionswitch;
     })
-
 
 };
 
