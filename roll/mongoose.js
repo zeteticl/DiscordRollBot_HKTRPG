9ea8ff28684ba04077259353c1e51180248b48f4
zeteticl <cougar.l@hotmail.com>
2018-03-14 16:39:44 +0800

    312321

--- roll/mongoose.js
+++ roll/mongoose.js
@@ -6,23 +6,25 @@ var rply = { type: 'text' }; //type是必需的,但可以更改
 //////////////// 
 ////////////////////////////////////////		
 
- function switchfind(id, name) {
+function switchfind(id, name) {
+    var check;
     mongodb.functionSwitch.findOne({ groupid: id, functionname: name }, function (err, data) {
         if (err) {
             console.log(err);
             return;
         }
 
-       else if (!data) {
+        else if (!data) {
             console.log("No record found")
             return;
         }
         else
-        console.log('date = ' + data);
+            console.log('date = ' + data);
+            check = data;
         //console.log('data.functionswitch = ' + data.functionswitch);
         return '0';
     })
-    return data;
+    return check;
 };
 
 module.exports = {
