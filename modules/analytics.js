038b1ba876a1fdba0e5a5a2a1486af294c0f93df
zeteticl <cougar.l@hotmail.com>
2018-03-19 13:14:38 +0800

    ?

--- modules/analytics.js
+++ modules/analytics.js
@@ -9,7 +9,7 @@ require('fs').readdirSync('./roll/').forEach(function (file) {
 
 //用來呼叫骰組,新增骰組的話,要寫條件式到下面呼叫 
 //格式是 exports.骰組檔案名字.function名
-function parseInput(rplyToken, inputStr, callback) {
+async function parseInput(rplyToken, inputStr, callback) {
 	//console.log('InputStr: ' + inputStr);
 	_isNaN = function (obj) {
 		return isNaN(parseInt(obj));
@@ -20,7 +20,7 @@ function parseInput(rplyToken, inputStr, callback) {
 	let trigger = mainMsg[0].toString().toLowerCase(); //指定啟動詞在第一個詞&把大階強制轉成細階
 
 
-	if (X(inputStr) == "STOP") return;
+	if  (X(inputStr) == "STOP") return;
 	//在下面位置開始分析trigger
 
 	//普通ROLL擲骰判定在此	
