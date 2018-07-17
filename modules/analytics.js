7d0aa7982452bd32c542801d614ffc16ec3e0c01
zeteticl <cougar.l@hotmail.com>
2018-07-17 16:42:25 +0800

    aaa

--- modules/analytics.js
+++ modules/analytics.js
@@ -75,10 +75,10 @@ function parseInput(rplyToken, inputStr, groupid) {
 
 
 	//choice 指令開始於此
-	if (trigger.match(/choice|隨機|選項|選1/) != null && mainMsg.length >= 3&&exports.mongoose.onOff(groupid,"隨機")==0) return exports.funny.choice(inputStr, mainMsg);
+	if (trigger.match(/choice|隨機|選項|選1/) != null && mainMsg.length >= 3&&exports.mongoose.onOff(groupid,"隨機")!=0) return exports.funny.choice(inputStr, mainMsg);
 
 	//tarot 指令
-	if (trigger.match(/tarot|塔羅牌|塔羅/) != null&&exports.mongoose.onOff(groupid,"塔羅")==0) {
+	if (trigger.match(/tarot|塔羅牌|塔羅/) != null&&exports.mongoose.onOff(groupid,"塔羅")!=0) {
 		if (trigger.match(/^單張|^每日|^daily/) != null) return exports.funny.NomalDrawTarot(mainMsg[1], mainMsg[2]);//預設抽 79 張
 		if (trigger.match(/^時間|^time/) != null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
 		if (trigger.match(/^大十字|^cross/) != null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 2);
