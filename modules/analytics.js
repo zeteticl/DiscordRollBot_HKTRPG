69cdcfae321d01fd3da2f55780ea91ca6b5c08a0
zeteticl <cougar.l@hotmail.com>
2018-08-17 17:10:15 +0800

    QQ

--- modules/analytics.js
+++ modules/analytics.js
@@ -91,7 +91,7 @@ function parseInput(rplyToken, inputStr, groupid) {
 	}
 	//tarot 指令
 	if (trigger.match(/tarot|塔羅牌|塔羅/) != null &&   exports.mongoose.onOff(groupid, "塔羅") != "0") {
-		const result = await exports.mongoose.onOff(groupid, "塔羅");
+		let result = await exports.mongoose.onOff(groupid, "塔羅");
 		console.log(result);
 		if (trigger.match(/^單張|^每日|^daily/) != null) return exports.funny.NomalDrawTarot(mainMsg[1], mainMsg[2]);//預設抽 79 張
 		if (trigger.match(/^時間|^time/) != null) return exports.funny.MultiDrawTarot(mainMsg[1], mainMsg[2], 1);
