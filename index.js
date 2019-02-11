7d1bec3d5fb9daa6892d2fbc1925c3953ed1aedc
zeteticl <cougar.l@hotmail.com>
2019-02-11 21:32:05 +0800

    qaq

--- index.js
+++ index.js
@@ -3,121 +3,43 @@ var bodyParser = require('body-parser');
 var app = express();
 var jsonParser = bodyParser.json();
 var channelSecret = process.env.DISCORD_CHANNEL_SECRET;
-const Discord = require('discord.js');
-const client = new Discord.Client();
 // Load `*.js` under modules directory as properties
 //  i.e., `User.js` will become `exports['User']` or `exports.User`
 require('fs').readdirSync(__dirname + '/modules/').forEach(function (file) {
-	if (file.match(/\.js$/) !== null && file !== 'index.js') {
-		var name = file.replace('.js', '');
-		exports[name] = require('./modules/' + file);
-	}
+  if (file.match(/\.js$/) !== null && file !== 'index.js') {
+    var name = file.replace('.js', '');
+    exports[name] = require('./modules/' + file);
+  }
 });
-
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+  console.log('Ready!');
 });
 
 client.login(channelSecret);
 
 client.on('message', message => {
-	console.log(message.content);
+  console.log(message.content);
 
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
-		message.channel.send('Pong.');
-	}
-
-let rplyVal = {};
-	//訊息來到後, 會自動呼叫handleEvent 分類,然後跳到analytics.js進行骰組分析
-	//如希望增加修改骰組,只要修改analytics.js的條件式 和ROLL內的骰組檔案即可,然後在HELP.JS 增加說明.
-	try {
-		rplyVal = exports.analytics.parseInput(message.content);
-	} catch (e) {
-		console.log('catch error');
-		console.log('Request error: ' + e.message);
-	}
-	if (rplyVal) {
-		//exports.replyMsgToLine.replyMsgToLine(rplyToken, rplyVal, options);
-		message.channel.send(rplyVal.text);
-		console.log(rplyVal);
-	} else {
-		console.log('Do not trigger');
-	}
-});
-
-/*
-var options = {
-	host: 'api.line.me',
-	port: 443,
-	path: '/v2/bot/message/reply',
-	method: 'POST',
-	headers: {
-	'Content-Type': 'application/json',
-	'Authorization':'Bearer ' + channelAccessToken
-	}
-}
-app.set('port', (process.env.PORT || 5000));
-// views is directory for all template files
-app.get('/', function(req, res) {
-//	res.send(parseInput(req.query.input));
-	res.send('Hello');
-});
-app.post('/', jsonParser, function(req, res) {
-	let event = req.body.events[0];
-	let type = event.type;
-	let msgType = event.message.type;
-	let msg = event.message.text;
-	let rplyToken = event.replyToken;
-	let rplyVal = {};
-	//console.log(msg);
-	//訊息來到後, 會自動呼叫handleEvent 分類,然後跳到analytics.js進行骰組分析
-	//如希望增加修改骰組,只要修改analytics.js的條件式 和ROLL內的骰組檔案即可,然後在HELP.JS 增加說明.
-	try {
-	rplyVal = handleEvent(event);
-	}
-	catch(e) {
-		console.log('catch error');
-		console.log('Request error: ' + e.message);
-	}
-	//把回應的內容,掉到replyMsgToLine.js傳出去
-	if (rplyVal) {
-	exports.replyMsgToLine.replyMsgToLine(rplyToken, rplyVal, options);
-	} else {
-	//console.log('Do not trigger');
-	}
-	res.send('ok');
-});
-
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
-});
-
-function handleEvent(event) {
-  switch (event.type) {
-    case 'message':
-      const message = event.message;
-      switch (message.type) {
-        case 'text':
-          return exports.analytics.parseInput(event.rplyToken, event.message.text);
-        default:
-           break;
-      }
-    case 'follow':
-		break;
-    case 'unfollow':
-       break;
-    case 'join':
-break;
-    case 'leave':
-       break;
-    case 'postback':
-       break;
-    case 'beacon':
-      break;
-    default:
-       break;
+  if (message.content === '!ping') {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send('Pong.');
   }
-}
-*/
+  let rplyVal = {};
+
+  try {
+    rplyVal = exports.analytics.parseInput(message.content);
+  } catch (e) {
+    console.log('catch error');
+    console.log('Request error: ' + e.message);
+  }
+  //把回應的內容,掉到replyMsgToLine.js傳出去
+  if (rplyVal) {
+    message.channel.send(rplyVal);
+  } else {
+    //console.log('Do not trigger'); 
+  }
+ // res.send('ok');
+});
