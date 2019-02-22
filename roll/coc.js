5c2bb26fd0563f86a184c154b81ec5612d650aa1
zeteticl <cougar.l@hotmail.com>
2019-02-22 12:58:08 +0800

    v1.3.3

--- roll/coc.js
+++ roll/coc.js
@@ -305,13 +305,13 @@ function coc6(chack, text) {
 
 function coc7(chack, text) {
 	let temp = rollbase.Dice(100);
-	if (temp > chack) rply.text = '1D100 → ' + chack + "：\n" + temp + ' → 失敗';
-	if (temp <= chack) rply.text = '1D100 → ' + chack + "：\n" + temp + ' → 通常成功';
-	if (temp <= chack / 2) rply.text = '1D100 → ' + chack + "：\n" + temp + ' → 困難成功';
-	if (temp <= chack / 5) rply.text = '1D100 → ' + chack + "：\n" + temp + ' → 極限成功';
-	if (temp == 1) rply.text = '1D100 → ' + chack + "：\n" + temp + ' → 恭喜！大成功！';
-	if (temp == 100) rply.text = '1D100 → ' + chack + "：\n" + temp + ' → 啊！大失敗！';
-	if (temp >= 96 && chack <= 49) rply.text = '1D100 → ' + chack + "：\n" + temp + ' → 啊！大失敗！';
+	if (temp > chack) rply.text = '1D100 ＜＝ ' + chack + "：\n" + temp + ' → 失敗';
+	if (temp <= chack) rply.text = '1D100 ＜＝ ' + chack + "：\n" + temp + ' → 通常成功';
+	if (temp <= chack / 2) rply.text = '1D100 ＜＝ ' + chack + "：\n" + temp + ' → 困難成功';
+	if (temp <= chack / 5) rply.text = '1D100 ＜＝ ' + chack + "：\n" + temp + ' → 極限成功';
+	if (temp == 1) rply.text = '1D100 ＜＝ ' + chack + "：\n" + temp + ' → 恭喜！大成功！';
+	if (temp == 100) rply.text = '1D100 ＜＝ ' + chack + "：\n" + temp + ' → 啊！大失敗！';
+	if (temp >= 96 && chack <= 49) rply.text = '1D100 ＜＝ ' + chack + "：\n" + temp + ' → 啊！大失敗！';
 	if (text != null) rply.text += '：' + text;
 	return rply;
 }
@@ -351,7 +351,7 @@ function coc7bp(chack, bpdiceNum, text) {
 		countStr = countStr.substring(0, countStr.length - 1)
 		let countArr = countStr.split('、');
 		countStr = countStr + ' → ' + coc7chack(Math.min(...countArr), chack, text);
-		rply.text = '1D100 → ' + chack + "：\n" + countStr;
+		rply.text = '1D100 ＜＝ ' + chack + "：\n" + countStr;
 		return rply;
 	}
 
@@ -366,7 +366,7 @@ function coc7bp(chack, bpdiceNum, text) {
 		countStr = countStr.substring(0, countStr.length - 1)
 		let countArr = countStr.split('、');
 		countStr = countStr + ' → ' + coc7chack(Math.max(...countArr), chack, text);
-		rply.text = chack + '\n' + countStr;
+		rply.text = '1D100 ＜＝ ' + chack + "：\n" + countStr;
 		return rply;
 	}
 }
